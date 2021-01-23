14d9fcbb86903e45461705de8c2002747d07310f
Jessie Cruz <jac237@ucsd.edu>
2021-01-23 15:36:16 -0800

    App.jsx: Included Navbar in Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,20 @@ import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import '@audius/stems/dist/stems.css';
 import '@audius/stems/dist/avenir.css';
-import LoadingGrid from './components/LoadingGrid';
 
-// import Main from './components/UserPage';
-const HomePage = lazy(() => import('./routes/HomePage'));
-const UserPage = lazy(() => import('./routes/UserPage'));
+import Navbar from './components/Navbar';
+import LoadingGrid from './components/LoadingGrid';
+const Home = lazy(() => import('./routes/Home'));
+const User = lazy(() => import('./routes/User'));
 
 function App() {
   return (
-    // React Router + Switch:
     <Router>
-      <Suspense fallback={<div><LoadingGrid /></div>}>
+      <Navbar />
+      <Suspense fallback={<LoadingGrid />}>
         <Switch>
-          <Route exact path="/" component={HomePage}/>
-          <Route path="/:handle" component={UserPage}/>
+          <Route exact path="/" component={Home}/>
+          <Route path="/:handle" component={User}/>
         </Switch>
       </Suspense>
     </Router>
