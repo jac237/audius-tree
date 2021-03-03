6bdc88cdf96910895a98f8712b3167986d01ed54
Jessie Cruz <jac237@ucsd.edu>
2021-03-02 20:17:27 -0800

    removed gql imports

--- client/src/components/FeaturedArtists.jsx
+++ client/src/components/FeaturedArtists.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/prop-types */
-import { useQuery, gql } from '@apollo/client';
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
--- client/src/components/Track/TrackTileCard.jsx
+++ client/src/components/Track/TrackTileCard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { gql, useLazyQuery } from '@apollo/client';
+import { useLazyQuery } from '@apollo/client';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
