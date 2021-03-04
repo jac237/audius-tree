93478326727493c90c876723f2f49504f1b517d6
Jessie Cruz <jac237@ucsd.edu>
2021-03-03 21:17:59 -0800

    apolloprovider: locahost/production api uri fix

--- client/src/ApolloProvider.jsx
+++ client/src/ApolloProvider.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import App from './App';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
+const API_URI =
+  window.location.hostname === 'localhost'
+    ? 'http://localhost:1337/graphql'
+    : 'https://audiustree-graphql.vercel.app/graphql';
+
 const client = new ApolloClient({
-  uri: 'http://localhost:1337/graphql',
+  uri: API_URI,
   cache: new InMemoryCache(),
 });
 
